22519d22b574df441f76ce5ccd4be4d35e3b6979
uhooi <the_uhooi@yahoo.co.jp>
2024-04-12 22:42:41 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,24 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LogDog",
+    platforms: [
+            .iOS(.v16),
+            .macOS(.v13),
+            .tvOS(.v16),
+            .watchOS(.v9),
+            .visionOS(.v1),
+        ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LogDog",
             targets: ["LogDog"]),
     ],
+    dependencies: [
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "LogDog"),
         .testTarget(
