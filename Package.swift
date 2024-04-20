add2ec0d891e4caae9eb1eeafec51926a9d28e16
uhooi <the_uhooi@yahoo.co.jp>
2024-04-20 12:04:22 +0900

    Add swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
             targets: ["LogdogUI"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
     ],
     targets: [
         .target(
